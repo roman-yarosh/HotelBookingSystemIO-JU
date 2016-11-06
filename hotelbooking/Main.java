c5d6e2577161398ec88c0571d262844462356940
roman-yarosh <yracompany@gmail.com>
2016-11-06 14:12:15 +0200

    Hotel

--- hotelbooking/Main.java
+++ hotelbooking/Main.java
@@ -17,18 +17,20 @@ public class Main {
         User user2 = new User(2, "Petr", "Petrov");
         User user3 = new User(3, "Stepan", "Stepanov");
 
+        printMessage("Register user1 (controller.registerUser(user1)).");
         controller.registerUser(user1);
+        printMessage("Register user2 (controller.registerUser(user2)).");
         controller.registerUser(user2);
 
-        printMessage("Try to print list of registered users when current user is not set!");
+        printMessage("Try to print list of registered users (controller.getAllUsers()) when current user is not set!");
         try {
             controller.getAllUsers().forEach(System.out::println);
         } catch (NullPointerException e) {
             printMessage("Try/Catch in main() method from Main class: NullPointerException! Current user is not set so getAllUsers() method returns null!");
         }
 
-        printMessage("Try to print list of registered users when current user is set to not registered user3!");
-        printMessage("Set user3 as current user.");
+        printMessage("Try to print list of registered users (controller.getAllUsers()) when current user is set to not registered user3!");
+        printMessage("Set user3 as current user (currentUser.setCurrentUser(user3)).");
         currentUser.setCurrentUser(user3);
         try {
             controller.getAllUsers().forEach(System.out::println);
@@ -36,10 +38,10 @@ public class Main {
             printMessage("Try/Catch in main() method from Main class: NullPointerException! Current user is user3 but he is not registered so getAllUsers() method returns null!");
         }
 
-        printMessage("Try to print list of registered users when current user is set to registered user2!");
-        printMessage("Set user2 as current user.");
+        printMessage("Try to print list of registered users (controller.getAllUsers()) when current user is set to registered user2!");
+        printMessage("Set user2 as current user (currentUser.setCurrentUser(user2)).");
         currentUser.setCurrentUser(user2);
-        printMessage("Registered users:");
+        printMessage("Registered users (controller.getAllUsers()):");
         try {
             controller.getAllUsers().forEach(System.out::println);
         } catch (NullPointerException e) {
@@ -47,43 +49,49 @@ public class Main {
         }
 
         try {
-            printMessage("Print all hotels from DAO");
+            printMessage("Print all hotels from DAO (controller.getAllHotels())");
  //           controller.getAllHotels().forEach(System.out::println);
         } catch (NullPointerException e) {
             printMessage("Try/Catch in main() method from Main class: NullPointerException! Current user is is not registered so getAllHotels() method returns null!");
         }
 
         try {
-            printMessage("Print all hotels in Kiev city!");
+            printMessage("Print all hotels in Kiev city (controller.findHotelByCity(\"Kiev\"))!");
             controller.findHotelByCity("Kiev").forEach(System.out::println);
-            printMessage("Print all hotels in Odessa city!");
-            controller.findHotelByCity("Odessa").forEach(System.out::println);
-            printMessage("Print all hotels in Lvov city!");
-            controller.findHotelByCity("Lvov").forEach(System.out::println);
+            printMessage("Print all hotels in Nikolaev city (controller.findHotelByCity(\"Nikolaev\"))!");
+            controller.findHotelByCity("Nikolaev").forEach(System.out::println);
+            printMessage("Print all hotels by hotel name Bristol (controller.findHotelByName(\"Bristol\"))!");
+            controller.findHotelByName("Bristol").forEach(System.out::println);
+            printMessage("Print all hotels by hotel name Hilton (controller.findHotelByName(\"Hilton\"))!");
+            controller.findHotelByName("Hilton").forEach(System.out::println);
         } catch (Exception e) {
             printMessage("Try/Catch in main() method from Main class: NullPointerException! There are no registered users or current user is not set so findHotelByCity() method returns null!");
         }
-        //controller.findHotelByCity("Kiev").forEach(System.out::println);
-
-
-
-        //controller.findHotelByName("Leopolis").forEach(System.out::println);
-        //controller.findHotelByName("Hyatt").forEach(System.out::println);
-        //controller.findHotelByName("Nobilis").forEach(System.out::println);
-
 
         Map<String, String> map = new HashMap<>();
         map.put("persons", "2");
-        //map.put("price", "300");
-        //map.put("city", "Kiev");
-        //map.put("hotelName", "Radisson");
+        map.put("price", "300");
+        map.put("city", "Kiev");
+        map.put("hotelName", "Radisson");
         try {
+            printMessage("Print all hotels by given parameters in map persons = 2, price = 300, city = 'Kiev', hotelName = 'Radisson' (controller.findRoom(map))!");
             controller.findRoom(map).forEach(System.out::print);
         } catch (NullPointerException e) {
             printMessage("Try/Catch in main() method from Main class: NullPointerException! There are no registered users or current user is not set so findRoom() method returns null!");
         }
 
-        //controller.getAllHotels().forEach(System.out::println);
+        map.clear();
+        //map.put("persons", "2");
+        map.put("price", "100");
+        map.put("city", "Kiev");
+        //map.put("hotelName", "Radisson");
+        try {
+            printMessage("Print all hotels by given parameters in map price = 100, city = 'Kiev' (controller.findRoom(map))!");
+            controller.findRoom(map).forEach(System.out::print);
+        } catch (NullPointerException e) {
+            printMessage("Try/Catch in main() method from Main class: NullPointerException! There are no registered users or current user is not set so findRoom() method returns null!");
+        }
+
 
         long hotelId = 6;
         long userId = 2;
