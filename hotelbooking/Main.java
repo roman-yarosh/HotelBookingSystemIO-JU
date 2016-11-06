a6ea04bd5fcce2d682385e89709d3b803abe710e
roman-yarosh <yracompany@gmail.com>
2016-11-06 22:51:25 +0200

    Hotel

--- hotelbooking/Main.java
+++ hotelbooking/Main.java
@@ -9,7 +9,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-
         System.out.println("Hotel Online Booking System!");
         Controller controller = new Controller();
         CurrentUser currentUser = CurrentUser.getInstance();
