94d4f4f3eeacc04dab8317144a7d2fa1096e01d9
roman-yarosh <yracompany@gmail.com>
2016-11-06 14:24:12 +0200

    Hotel

--- hotelbooking/Main.java
+++ hotelbooking/Main.java
@@ -92,7 +92,6 @@ public class Main {
             printMessage("Try/Catch in main() method from Main class: NullPointerException! There are no registered users or current user is not set so findRoom() method returns null!");
         }
 
-
         long hotelId = 6;
         long userId = 2;
         long roomId = 55;
@@ -122,5 +121,97 @@ public class Main {
         } catch (NullPointerException e) {
             System.out.println("Found 0 hotels in Kiev.");
         }*/
+
+
+        /*System output:
+        Hotel Online Booking System!
+
+        Register user1 (controller.registerUser(user1)).
+
+        Register user2 (controller.registerUser(user2)).
+
+        Try to print list of registered users (controller.getAllUsers()) when current user is not set!
+
+        Try/Catch in isUserRegistered() method from the Controller class: NullPointerException! Current user is not set!
+
+        Try/Catch in main() method from Main class: NullPointerException! Current user is not set so getAllUsers() method returns null!
+
+        Try to print list of registered users (controller.getAllUsers()) when current user is set to not registered user3!
+
+        Set user3 as current user (currentUser.setCurrentUser(user3)).
+
+        Current user is User{userId=3, firstName='Stepan', lastName='Stepanov', userRegistered=false}
+
+        Current user is not registered! Please, register user in the system! User{userId=3, firstName='Stepan', lastName='Stepanov', userRegistered=false}
+
+        Try/Catch in main() method from Main class: NullPointerException! Current user is user3 but he is not registered so getAllUsers() method returns null!
+
+        Try to print list of registered users (controller.getAllUsers()) when current user is set to registered user2!
+
+        Set user2 as current user (currentUser.setCurrentUser(user2)).
+
+        Current user is User{userId=2, firstName='Petr', lastName='Petrov', userRegistered=true}
+
+        Registered users (controller.getAllUsers()):
+        User{userId=1, firstName='Ivan', lastName='Ivanov', userRegistered=true}
+        User{userId=2, firstName='Petr', lastName='Petrov', userRegistered=true}
+
+        Print all hotels from DAO (controller.getAllHotels())
+
+        Print all hotels in Kiev city (controller.findHotelByCity("Kiev"))!
+
+        Hotel{hotelId=1, hotelName='Radisson Hotel', hotelCity='Kiev', hotelRooms=[
+        Room{roomId=1, persons=1, price=100.0, userReserved=null},
+        Room{roomId=2, persons=1, price=150.0, userReserved=null},
+        Room{roomId=3, persons=1, price=200.0, userReserved=null},
+        Room{roomId=4, persons=2, price=250.0, userReserved=null},
+        Room{roomId=5, persons=2, price=300.0, userReserved=null},
+        Room{roomId=6, persons=2, price=350.0, userReserved=null},
+        Room{roomId=7, persons=3, price=400.0, userReserved=null},
+        Room{roomId=8, persons=3, price=450.0, userReserved=null},
+        Room{roomId=9, persons=3, price=500.0, userReserved=null},
+        Room{roomId=10, persons=3, price=550.0, userReserved=null}]}
+
+        Hotel{hotelId=2, hotelName='Hyatt Regency Hotel', hotelCity='Kiev', hotelRooms=[
+        Room{roomId=11, persons=1, price=100.0, userReserved=null},
+        Room{roomId=12, persons=1, price=150.0, userReserved=null},
+        Room{roomId=13, persons=1, price=200.0, userReserved=null},
+        Room{roomId=14, persons=2, price=250.0, userReserved=null},
+        Room{roomId=15, persons=2, price=300.0, userReserved=null},
+        Room{roomId=16, persons=2, price=350.0, userReserved=null},
+        Room{roomId=17, persons=3, price=400.0, userReserved=null},
+        Room{roomId=18, persons=3, price=450.0, userReserved=null},
+        Room{roomId=19, persons=3, price=500.0, userReserved=null},
+        Room{roomId=20, persons=3, price=550.0, userReserved=null}]}
+
+        Print all hotels in Nikolaev city (controller.findHotelByCity("Nikolaev"))!
+
+        Print all hotels by hotel name Bristol (controller.findHotelByName("Bristol"))!
+
+        Hotel{hotelId=6, hotelName='Bristol Hotel', hotelCity='Odessa', hotelRooms=[
+        Room{roomId=51, persons=1, price=100.0, userReserved=null},
+        Room{roomId=52, persons=1, price=150.0, userReserved=null},
+        Room{roomId=53, persons=1, price=200.0, userReserved=null},
+        Room{roomId=54, persons=2, price=250.0, userReserved=null},
+        Room{roomId=55, persons=2, price=300.0, userReserved=null},
+        Room{roomId=56, persons=2, price=350.0, userReserved=null},
+        Room{roomId=57, persons=3, price=400.0, userReserved=null},
+        Room{roomId=58, persons=3, price=450.0, userReserved=null},
+        Room{roomId=59, persons=3, price=500.0, userReserved=null},
+        Room{roomId=60, persons=3, price=550.0, userReserved=null}]}
+
+        Print all hotels by hotel name Hilton (controller.findHotelByName("Hilton"))!
+
+        Print all hotels by given parameters in map persons = 2, price = 300, city = 'Kiev', hotelName = 'Radisson' (controller.findRoom(map))!
+
+        Hotel{hotelId=1, hotelName='Radisson Hotel', hotelCity='Kiev', hotelRooms=[
+        Room{roomId=5, persons=2, price=300.0, userReserved=null}]}
+        Print all hotels by given parameters in map price = 100, city = 'Kiev' (controller.findRoom(map))!
+
+        Hotel{hotelId=1, hotelName='Radisson Hotel', hotelCity='Kiev', hotelRooms=[
+        Room{roomId=1, persons=1, price=100.0, userReserved=null}]}
+        Hotel{hotelId=2, hotelName='Hyatt Regency Hotel', hotelCity='Kiev', hotelRooms=[
+        Room{roomId=11, persons=1, price=100.0, userReserved=null}]}
+         */
     }
 }
